d6e57b393446db95535b47dc8ac859d89eb6fe8b
tashbeeh25 <tashbeiph@gmail.com>
2020-08-21 09:18:31 +0100

    test file for InputPage

--- src/components/InputPage.test.js
+++ src/components/InputPage.test.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { shallow, mount } from 'enzyme';
 import InputPage from './InputPage';
-import renderer from 'react-test-renderer';
+
 describe('<Form />', () => {
+
     it('should render a form', () => {
         const wrapper = shallow(<InputPage />); 
         expect(wrapper.exists('form')).toEqual(true);
     })
+
     it('should have 3 select options', () => {
         const wrapper = shallow(<InputPage />); 
         expect(wrapper.find('select').length).toEqual(2);
     })
+
+    it('On change the value should change', () => {
+        const wrapper = mount(<BrowserRouter><InputPage /></BrowserRouter>); 
+        wrapper.find('select').at(0).simulate('change', { target: { name: 'playersNumber', value: "easy" } });
+        expect(wrapper.find('select').at(0).prop('value')).toEqual("easy");
+    })
+
 });
+
+
